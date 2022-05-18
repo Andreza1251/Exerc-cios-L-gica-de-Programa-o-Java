3da6a956c4ed08641fc860ef90f31fc8dc3cb202
Andreza Morais <andrezamorais2090@gmail.com>
2022-05-18 13:46:09 -0300

    exercicio 6

---
+++ src/logicaexerciciosiii/Exercicio06.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicaexerciciosiii;
+
+
+import java.util.Scanner;
+
+
+public class LogicaExerciciosIII {
+
+//Resolução do exercício 6
+   
+  public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+     
+        
+       int variavelA;
+       int variavelB;
+       
+       
+       System.out.println("Digite o valor da variável A");
+       variavelA = leitor.nextInt();
+       
+        System.out.println("Digite o valor da variável B");
+       variavelB = leitor.nextInt();
+       
+      
+      
+      System.out.println("Agora a variável A vale " + variavelB + " e a variável B vale "+ variavelA);
+      
+      
+              
+       
+       
+  }
+    
+}
